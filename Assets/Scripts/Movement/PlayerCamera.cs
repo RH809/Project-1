195c86d39de581b9ee891e62228d563e760623ae
RH809 <nayr.oaish@gmail.com>
2026-05-15 19:36:38 -0700

    Added Walking + Camera input

---
+++ Assets/Scripts/Movement/PlayerCamera.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    [SerializeField] private Transform cameraTransform;
+
+    [SerializeField] private float sensitivity = 100f;
+    [SerializeField] private float maxYRotation = 60f;
+    [SerializeField] private float minYRotation = -80f;
+
+    private PlayerInput playerInput;
+    private Vector2 lookInput;
+    private float xRotation;
+
+    void Awake()
+    {
+        playerInput = new PlayerInput();
+
+        playerInput.Player.Look.performed += ctx =>
+        {
+            lookInput = ctx.ReadValue<Vector2>();
+        };
+
+        playerInput.Player.Look.canceled += ctx =>
+        {
+            lookInput = Vector2.zero;
+        };
+    }
+
+    void OnEnable()
+    {
+        playerInput.Enable();
+    }
+
+    void OnDisable()
+    {
+        playerInput.Disable();
+    }
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        float mouseX = lookInput.x * sensitivity * Time.deltaTime;
+        float mouseY = lookInput.y * sensitivity * Time.deltaTime;
+
+        // Rotate player left/right
+        transform.Rotate(Vector3.up * mouseX);
+
+        // Rotate camera up/down
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, minYRotation, maxYRotation);
+
+        cameraTransform.localRotation =
+            Quaternion.Euler(xRotation, 0f, 0f);
+    }
+}
---
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] private Animator playerAnimator;
+
+    [SerializeField] private float sprintSpeed;
+    [SerializeField] private float walkSpeed;
+
+    private PlayerInput playerInput;
+    private Vector2 moveInput;
+    private bool sprinting = false;
+
+    private Rigidbody rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        playerInput = new PlayerInput();
+        rb = GetComponent<Rigidbody>();
+
+        playerInput.Player.Move.performed += ctx =>
+        {
+            moveInput = ctx.ReadValue<Vector2>();
+            if (sprinting)
+            {
+                playerAnimator.SetFloat("Speed", 2.0f);
+            }
+            else
+            {
+                playerAnimator.SetFloat("Speed", 1.0f);
+            }
+        };
+        playerInput.Player.Move.canceled += ctx =>
+        {
+            moveInput = Vector2.zero;
+            playerAnimator.SetFloat("Speed", 0.0f);
+        };
+
+        playerAnimator.SetFloat("Speed", 0.0f);
+    }
+
+    void OnEnable() {
+        playerInput.Enable();
+    }
+
+    void OnDisable() {
+        playerInput.Disable();
+    }
+
+    void FixedUpdate() {
+        Vector3 movement = new Vector3(
+            moveInput.x,
+            0,
+            moveInput.y
+        );
+
+        movement = transform.TransformDirection(movement);
+        rb.MovePosition(rb.position + movement * (sprinting ? sprintSpeed : walkSpeed) * Time.fixedDeltaTime);
+
+    }
+}
