30b20667beb89538c93a0754d9b232bd0d9ed423
RH809 <nayr.oaish@gmail.com>
2026-05-20 15:20:00 -0700

    Added disruptor health

---
+++ Assets/Scripts/Entities/Disruptor.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class Disruptor : MonoBehaviour
+{
+    [SerializeField] private Animator disruptorAnimator;
+
+    private Health health;
+
+    private bool alive = true;
+    public bool isAlive { get => alive; }
+
+
+    void Start()
+    {
+        health = GetComponent<Health>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("t")) {
+            if (alive) health.TakeDamage(100, gameObject);
+            else Respawn();
+        }
+    }
+
+    void OnEnable()
+    {
+        Health.OnDie += Die;
+    }
+
+    void OnDisable()
+    {
+        Health.OnDie -= Die;
+    }
+
+    void Die(HealthContext healthContext)
+    {
+        if (healthContext.target == gameObject) {
+            disruptorAnimator.SetTrigger("Die");
+            alive = false;
+        }
+    }
+
+    void Respawn()
+    {
+        disruptorAnimator.SetTrigger("Respawn");
+        health.Respawn();
+        alive = true;
+    }
+}
---
+++ Assets/Scripts/Entities/Health.cs
@@ -0,0 +1,66 @@
+using System;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int maxHealth;
+    [SerializeField] private int baseRegen;
+    [SerializeField] private float regenRate;
+
+    private int currentHealth;
+    private float regenTimer;
+
+    public static event Action<HealthContext> OnHeal;
+    public static event Action<HealthContext> OnTakeDamage;
+    public static event Action<HealthContext> OnDie;
+
+    private HealthContext healthContext;
+    void Start()
+    {
+        currentHealth = maxHealth;
+        regenTimer = regenRate;
+
+        healthContext.target = gameObject;
+    }
+
+    void Update()
+    {
+        regenTimer -= Time.deltaTime;
+        if (regenTimer <= 0) {
+            regenTimer = regenRate;
+            currentHealth = Mathf.Min(currentHealth + baseRegen, maxHealth);
+        }
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+    }
+
+    public void Heal(int healAmount)
+    {
+        currentHealth = Mathf.Min(currentHealth + healAmount, maxHealth);
+        healthContext.source = gameObject;
+        OnHeal?.Invoke(healthContext);
+    }
+
+    public void TakeDamage(int damageAmount, GameObject attacker)
+    {
+        currentHealth = Mathf.Max(currentHealth - damageAmount, 0);
+        healthContext.source = attacker;
+        OnTakeDamage?.Invoke(healthContext);
+        if (currentHealth == 0)
+        {
+            OnDie?.Invoke(healthContext);
+        }
+    }
+
+    public void Respawn()
+    {
+        currentHealth = maxHealth;
+        regenTimer = regenRate;
+    }   
+    
+}
+
+public struct HealthContext
+{
+    public GameObject source;
+    public GameObject target;
+}
