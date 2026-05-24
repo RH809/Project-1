2cf1bb60a7ba7bf0580138934772bd5815fdbce4
RH809 <nayr.oaish@gmail.com>
2026-05-24 12:42:44 -0700

    Fixed zombie movement

--- Assets/Scripts/Entities/Zombie/ZombieMovement.cs
+++ Assets/Scripts/Entities/Zombie/ZombieMovement.cs
@@ -2,6 +2,7 @@
 /// This script controls the pathfinding of a zombie.
 /// </summary>
 
+using System;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Apple;
@@ -12,8 +13,10 @@ public class ZombieMovement : MonoBehaviour
     private NavMeshAgent agent;
 
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float rotationSpeed;
     [SerializeField] private float attackRange;
-    [SerializeField] private float minTargetDist;
+    [SerializeField] private float playerStoppingDist;
+    [SerializeField] private float disruptorStoppingDist;
     [SerializeField] private float maxKiteRange; // kite range will scale inversely with distance from closest constructs
     [SerializeField] private float kiteRangeThreshold; // construct target must be outside of this range for player to take aggro
     [SerializeField] private float playerPriorityRange; // will not prioritize player if it is out of this range
@@ -39,9 +42,8 @@ public class ZombieMovement : MonoBehaviour
         attack = GetComponent<ZombieAttack>();
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
-        agent.updatePosition = false;
+        agent.updatePosition = true;
         agent.speed = moveSpeed;
-        agent.stoppingDistance = minTargetDist;
     }
 
     // Update is called once per frame
@@ -63,18 +65,88 @@ public class ZombieMovement : MonoBehaviour
         {
             // move toward target if necessary and allowed
             //rb.MovePosition(rb.position + rb.transform.forward * moveSpeed * Time.fixedDeltaTime);
+            
+            bool stopped = agent.velocity.magnitude < 0.01f;
+            //Debug.Log($"{target} {wasMoving} {agent.isStopped} {stopped} {changedTarget}");
+            if (changedTarget)
+            {
+                Vector3 destination = target.transform.position;
+                destination.y = rb.transform.position.y;
+                agent.SetDestination(destination);
+                Debug.Log("Starting movement toward " + destination + " | " + agent.destination);
+                zombieAnimator.ResetTrigger("Stop Moving");
+                zombieAnimator.SetTrigger("Start Moving");
+                wasMoving = true;
+            }
+            else
+            {
+                if (target.Equals(player))
+                {
+                    Vector3 destination = player.transform.position;
+                    destination.y = rb.transform.position.y;
+                    Debug.Log("Updating player tracking");
+                    //Debug.Log("Updating player tracking " + destination + " | " + agent.destination);
+                    agent.SetDestination(destination);
+                }
+                if (wasMoving)
+                {
+                    if (stopped)
+                    {
+                        Debug.Log("Stopping movement animation");
+                        zombieAnimator.ResetTrigger("Start Moving");
+                        zombieAnimator.SetTrigger("Stop Moving");
+                        wasMoving = false;
+                    }
+                    else
+                    {
+                        /*
+                        if (target.Equals(player))
+                        {
+                            Vector3 destination = player.transform.position;
+                            destination.y = rb.transform.position.y;
+                            Debug.Log("Updating player tracking");
+                            //Debug.Log("Updating player tracking " + destination + " | " + agent.destination);
+                            agent.SetDestination(destination);
+                        }
+                        else
+                        {
+                            Debug.Log($"Still tracking {target}");
+                        }
+                        */
+                        Debug.Log("Still moving");
+                        wasMoving = true;
+                    }
+                }
+                else
+                {
+                    if (!stopped)
+                    {
+                        Debug.Log("Starting movement from stop");
+                        zombieAnimator.ResetTrigger("Stop Moving");
+                        zombieAnimator.SetTrigger("Start Moving");
+                        wasMoving = true;
+                    }
+                    else
+                    {
+                        Debug.Log("Still not moving");
+                        wasMoving = false;
+                    }
+                }
+            }
+            /*
             float distanceToTarget = GetDistance(target);
-            if (wasMoving && distanceToTarget <= minTargetDist)
+            Debug.Log(agent.speed + " " + agent.isStopped + " " + wasMoving);
+            if (wasMoving && agent.isStopped) //distanceToTarget <= minTargetDist)
             {
                 // Stop movement if within min distance
-                agent.ResetPath();
-                Debug.Log("Stopping movement");
+                //agent.ResetPath();
+                Debug.Log("Stopping movement animation since too close");
                 zombieAnimator.SetTrigger("Stop Moving");
                 wasMoving = false;
             }
             else
             {
-                if ((!wasMoving || changedTarget) && distanceToTarget > minTargetDist)
+                if ((!wasMoving || changedTarget))
                 {
                     // Start movement toward target if either wasn't moving or changed target and outside of min target distance
                     
@@ -84,7 +156,7 @@ public class ZombieMovement : MonoBehaviour
                     Debug.Log("Starting movement toward " + destination + " | " + agent.destination);
                     zombieAnimator.SetTrigger("Start Moving");
                 }
-                else if (target.Equals(player) && distanceToTarget > minTargetDist)
+                else if (target.Equals(player))
                 { // Update target path if its player
                     Vector3 destination = player.transform.position;
                     destination.y = rb.transform.position.y;
@@ -93,6 +165,7 @@ public class ZombieMovement : MonoBehaviour
                 }
                 wasMoving = true;
             }
+            */
             
         }
         else
@@ -102,22 +175,42 @@ public class ZombieMovement : MonoBehaviour
                 // Stop movement entirely
                 Debug.Log("Stopping movement");
                 agent.ResetPath();
+                zombieAnimator.ResetTrigger("Start Moving");
                 zombieAnimator.SetTrigger("Stop Moving");
             }
             wasMoving = false;
         }
+
         // rotate to face target even if not moving
-        Debug.Log($"Target: {target}  {target.transform.position}  {agent.destination}");
+        //Debug.Log($"Target: {target}  {target.transform.position}  {agent.destination}");
+        Vector3 dir = target.transform.position - rb.transform.position;
+        dir.y = 0f;
+
+        if (dir.sqrMagnitude > 0.001f)
+        {
+            Quaternion targetRot = Quaternion.LookRotation(dir);
+
+            rb.MoveRotation(
+                Quaternion.Slerp(
+                    rb.rotation,
+                    targetRot,
+                    rotationSpeed * Time.fixedDeltaTime
+                )
+            );
+        }
+        /*
         Quaternion rotation = Quaternion.LookRotation(target.transform.position - rb.transform.position, Vector3.up);
         rb.MoveRotation(rotation);
+        */
         Debug.DrawRay(rb.transform.position, agent.destination - rb.transform.position, Color.red);
         Debug.DrawRay(rb.transform.position, target.transform.position - rb.transform.position, Color.orange);
-
+        /*
         // handle movement
         Vector3 targetPos = agent.steeringTarget;
         Vector3 dir = (targetPos - rb.position).normalized;
 
         rb.MovePosition(rb.position + dir * moveSpeed * Time.fixedDeltaTime);
+        */
     }
 
     /// <summary>
@@ -177,11 +270,16 @@ public class ZombieMovement : MonoBehaviour
         {
             changed = (target == null || !target.Equals(player));
             target = player;
+            agent.stoppingDistance = playerStoppingDist;
         }
         else
         {
             changed = (target == null || !target.Equals(closestConstruct));
             target = closestConstruct;
+            if (target.GetComponent<Disruptor>() != null)
+            {
+                agent.stoppingDistance = disruptorStoppingDist;
+            }
         }
         return changed;
     }
