13cac5fed07f609552af93d43cd21fe85f4a9f56
RH809 <nayr.oaish@gmail.com>
2026-07-01 17:57:09 -0700

    Stop player movement on game over

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -125,6 +125,7 @@ public class GameManager : Singleton<GameManager>
     void EndGame(bool win)
     {
         gameOver = true;
+        Player.Instance.Movement.StopMovement();
         if (win)
         {
 
